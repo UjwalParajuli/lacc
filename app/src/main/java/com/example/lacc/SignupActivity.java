93f974619e57a4574eeae4c2de7f3e40034327b5
shreesha khadka <shreesha.khadka.17@icp.edu.np>
2020-11-19 08:45:16 +0545

    sign commit

---
+++ app/src/main/java/com/example/lacc/SignupActivity.java
@@ -0,0 +1,26 @@
+package com.example.lacc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Spinner statespinner= (Spinner) findViewById(R.id.signupstate);
+
+        ArrayAdapter <String> stateAdapter = new ArrayAdapter<String>(SignupActivity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.State));
+        stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        statespinner.setAdapter(stateAdapter);
+
+
+    }
+
+}
