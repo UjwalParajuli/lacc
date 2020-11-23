7ef1625fc1cfa8aeab80ec41f1dcbc848cc6326c
Ujwal Parajuli <uzwalparajuli07@gmail.com>
2020-11-23 10:25:12 +0545

    Sign Up

--- app/src/main/java/com/example/lacc/SignUpActivity.java
+++ app/src/main/java/com/example/lacc/SignUpActivity.java
@@ -18,40 +18,10 @@ import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    String [] SPINNERLISTSTATE={"Gandaki Province", "Bagmati Province","Lumbini Province" };
-    String[] SPINNERLISTCITY={"Mustang","Kaski","Lamjung"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
-        TextView textView = findViewById(R.id.signup_login);
-
-        ArrayAdapter<String> arrayAdapterstate = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, SPINNERLISTSTATE);
-        MaterialBetterSpinner betterSpinner=(MaterialBetterSpinner) findViewById(R.id.signup_state);
-        betterSpinner.setAdapter(arrayAdapterstate);
-
-        ArrayAdapter<String> arrayAdaptercity = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, SPINNERLISTCITY);
-        MaterialBetterSpinner betterSpinner1=(MaterialBetterSpinner) findViewById(R.id.signup_city);
-        betterSpinner1.setAdapter(arrayAdaptercity);
-
-
-
-        String text ="Already have account?Log In";
-
-        SpannableString ss = new SpannableString(text);
-        ClickableSpan clickableSpan1=new ClickableSpan() {
-            @Override
-            public void onClick(@NonNull View view) {
-                Toast.makeText(SignUpActivity.this, "one", Toast.LENGTH_SHORT).show();
-            }
-        };
-        ss.setSpan(clickableSpan1,21, 27, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        textView.setText(ss);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
-
 
     }
 
--- app/src/main/java/com/example/lacc/WelcomeActivity.java
+++ app/src/main/java/com/example/lacc/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.example.lacc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +49,8 @@ public class WelcomeActivity extends AppCompatActivity {
 
     // Button Methods
     private void go_to_sign_up(){
+        Intent intent = new Intent(WelcomeActivity.this,SignUpActivity.class);
+        startActivity(intent);
 
     }
 
