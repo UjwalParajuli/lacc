f89d41834977e445665e6d909710d92ff154815b
Ujwal Parajuli <uzwalparajuli07@gmail.com>
2020-11-25 12:21:53 +0545

    Changes

--- app/src/main/java/com/example/lacc/VerificationCodeActivity.java
+++ app/src/main/java/com/example/lacc/VerificationCodeActivity.java
@@ -16,25 +16,25 @@ import android.widget.Toast;
 
 public class VerificationCodeActivity extends AppCompatActivity {
 
-    TextView Resendcode;
+//    TextView Resendcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification_code);
 
-        Resendcode = findViewById(R.id.verification_code_resend);
-
-        String text ="Did not Receive! Resend";
-
-        SpannableString ss = new SpannableString(text);
-        ClickableSpan clickableSpan1=new ClickableSpan() {
-            @Override
-            public void onClick(@NonNull View view) {
-                Toast.makeText(VerificationCodeActivity.this, "one", Toast.LENGTH_SHORT).show();
-            }
-        };
-        ss.setSpan(clickableSpan1,17, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        Resendcode.setText(ss);
-        Resendcode.setMovementMethod(LinkMovementMethod.getInstance());
+//        Resendcode = findViewById(R.id.verification_code_resend);
+//
+//        String text ="Did not Receive! Resend";
+//
+//        SpannableString ss = new SpannableString(text);
+//        ClickableSpan clickableSpan1=new ClickableSpan() {
+//            @Override
+//            public void onClick(@NonNull View view) {
+//                Toast.makeText(VerificationCodeActivity.this, "one", Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//        ss.setSpan(clickableSpan1,17, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        Resendcode.setText(ss);
+//        Resendcode.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
