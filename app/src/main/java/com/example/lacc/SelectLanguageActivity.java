2fb6d0ea8c7113b8368747e25683b7eabaa83b35
Ujwal Parajuli <uzwalparajuli07@gmail.com>
2020-11-29 19:09:25 +0545

    Select Language, Bottom Sheet, Settings

--- app/src/main/java/com/example/lacc/SelectLanguageActivity.java
+++ app/src/main/java/com/example/lacc/SelectLanguageActivity.java
@@ -3,19 +3,24 @@ package com.example.lacc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class SelectLanguageActivity extends AppCompatActivity {
+
     private ImageButton button_nepali_unselected, button_nepali_selected, button_english_unselected
             , button_english_selected;
-
     private ImageView image_checked_nepali, image_checked_english;
-
     private TextView tv_select_lang_en, tv_select_lang_np;
 
     @Override
@@ -111,7 +116,27 @@ public class SelectLanguageActivity extends AppCompatActivity {
     }
 
     private void button_continue(){
-        Intent intent = new Intent(SelectLanguageActivity.this,OnBoardingActivity.class);
-        startActivity(intent);
+        if (button_nepali_selected.getVisibility() == View.VISIBLE){
+            setAppLocale("ne");
+            Intent intent = new Intent(SelectLanguageActivity.this,OnBoardingActivity.class);
+            startActivity(intent);
+        }
+        if (button_english_selected.getVisibility() == View.VISIBLE){
+            setAppLocale("en");
+            Intent intent = new Intent(SelectLanguageActivity.this,OnBoardingActivity.class);
+            startActivity(intent);
+        }
+        if (button_nepali_unselected.getVisibility() == View.VISIBLE && button_english_unselected.getVisibility() == View.VISIBLE){
+            Toast.makeText(this, "Please select one language", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setAppLocale(String localeCode){
+        Resources resources = getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(new Locale(localeCode.toLowerCase()));
+        resources.updateConfiguration(configuration, displayMetrics);
+
     }
 }
