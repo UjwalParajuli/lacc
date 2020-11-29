1b56ed6ebf40ab417637ac077a58469a0b20fc24
snehagrg <snehagurunggurung@gmail.com>
2020-11-29 21:26:11 +0545

    ChatActivity

--- app/src/main/java/com/example/lacc/ChatActivity.java
+++ app/src/main/java/com/example/lacc/ChatActivity.java
@@ -3,20 +3,12 @@ package com.example.lacc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
-public class NotificationActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
-
-        Toolbar toolbar = findViewById(R.id.toolbar_notification);
-
+        setContentView(R.layout.activity_chat);
     }
-
-
-
-
 }
