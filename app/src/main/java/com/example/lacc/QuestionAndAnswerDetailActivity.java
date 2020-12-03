904517cb88d64313185eb5cf3500da083441b2f7
snehagrg <snehagurunggurung@gmail.com>
2020-12-03 21:06:27 +0545

    law_read_activity and question_and_answer_details_activity

---
+++ app/src/main/java/com/example/lacc/QuestionAndAnswerDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.lacc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionAndAnswerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_and_answer_detail);
+    }
+}
