060a62bd3f6a851aa4794e7f93d22a3a10fe73eb
snehagrg <snehagurunggurung@gmail.com>
2020-12-04 21:32:01 +0545

    Signup_validation

---
+++ app/src/main/java/com/example/lacc/InternetConnection.java
@@ -0,0 +1,37 @@
+package com.example.lacc;
+
+import android.content.Context;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class InternetConnection {
+    public static boolean isDeviceOnline(Context context) {
+
+        InetAddress inetAddress = null;
+        try {
+            Future<InetAddress> future = Executors.newSingleThreadExecutor().submit(new Callable<InetAddress>() {
+                @Override
+                public InetAddress call() {
+                    try {
+                        return InetAddress.getByName("google.com");
+                    } catch (UnknownHostException e) {
+                        return null;
+                    }
+                }
+            });
+            inetAddress = future.get(5000, TimeUnit.MILLISECONDS);
+            future.cancel(true);
+        } catch (InterruptedException e) {
+        } catch (ExecutionException e) {
+        } catch (TimeoutException e) {
+        }
+        return inetAddress != null && !inetAddress.equals("");
+    }
+}
--- app/src/main/java/com/example/lacc/SignUpActivity.java
+++ app/src/main/java/com/example/lacc/SignUpActivity.java
@@ -1,33 +1,102 @@
 package com.example.lacc;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextWatcher;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
 
 
     TextView Alreadylogin;
+    Button signUpBtnCreateAccount;
+    EditText fullName;
+    EditText phoneNumber;
+    EditText password;
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+
+                    "(?=.*[a-zA-Z])" +      //any letter
+                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{4,}" +               //at least 4 characters
+                    "$");
+
+    private  static  final Pattern Full_Name_Pattern = Pattern.compile("^"+"(?=.*[a-zA-Z])");
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        fullName = findViewById(R.id.sign_up_edit_text_full_name);
+        phoneNumber = findViewById(R.id.sign_up_edit_text_phone);
+        password = findViewById(R.id.sign_up_edit_text_password);
+
+        //validation
+        signUpBtnCreateAccount = findViewById(R.id.sign_up_button_create_account);
+        signUpBtnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String signUp_fullName = fullName.getText().toString();
+                String signUp_phone = phoneNumber.getText().toString();
+                String sign_up_password = password.getText().toString();
+
+                if(InternetConnection.isDeviceOnline(getApplicationContext())){
+                    if(signUp_fullName.length()==0){
+                        fullName.setError("Please enter your full name.");
+                        return;
+                    }
+
+                    if(!Full_Name_Pattern.matcher( signUp_fullName).matches()){
+                        password.setError("Full name must include only alphabet");
+                        return;
+                    }
+
+
+                    if(signUp_phone.length()==0){
+                        phoneNumber.setError("Please enter your phone number");
+                        return;
+                    }
+
+                    if(signUp_phone.length()<10 || phoneNumber.length()>10){
+                        phoneNumber.setError("Please enter valid number.");
+                    }
+
+                    if(sign_up_password.length()==0){
+                        password.setError("Please enter your password");
+                        return;
+                    }
+
+                    if(sign_up_password.length()<=5){
+                        password.setError("Password must be of at least 8 character");
+                        return;
+                    }
+
+                    if(!PASSWORD_PATTERN.matcher( sign_up_password).matches()){
+                        password.setError("Password too weak");
+                        return;
+
+                    }
+
+                    else{
+                        Toast.makeText(SignUpActivity.this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
 
         Alreadylogin = findViewById(R.id.signup_text_view_already_login);
 //        ETfullname = findViewById(R.id.signup_edit_text_full_name);
@@ -46,19 +115,7 @@ public class SignUpActivity extends AppCompatActivity {
         });
 
 
-        //for button
+
     }
-//    private boolean validatefullname()
-//    {
-//        String fullnameinput = ETfullname.getText().toString().trim();
-//        if (fullnameinput.isEmpty())
-//        {
-//            ETfullname.setError("Field cannot be empty");
-//            return false;
-//        }
-//        else {
-//            ETfullname.setError(null);
-//            return true;
-//        }
-//    }
+
 }
